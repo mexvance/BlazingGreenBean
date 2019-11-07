55e164407139af2dd93fb1b30f3afeb1b6d6ce24
Mike <mexvance04@gmail.com>
2019-11-07 15:30:58 -0700

    added some stuff to do things

---
+++ BlazingGreenBean/Services/GreenBeanAPIService.cs
@@ -0,0 +1,19 @@
+﻿using BlazingGreenBean.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingGreenBean.Services
+{
+    public class GreenBeanAPIService
+    {
+        public List<Recipe> GetRecipesByName(string recipeName)
+        {
+            //call api
+            //convert to recipes
+            //return recipes
+            return new List<Recipe> { };
+        }
+    }
+}
